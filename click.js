19bc7ab6a032e0a0b71f10fbeb9f740c227d6575
JonathanStevens17 <7JStevens@stpatrick.org>
2017-03-23 10:49:35 -0500

    Add files via upload

---
+++ click.js
@@ -0,0 +1,17 @@
+//declare samecolor() here
+function samecolor(index){
+  let newcolor = "F0FFF0"
+  const doc = Document;
+  let divs = doc.querySelector('#samecolor div');
+
+  divs[0].style.background = color;
+  divs[1].style.height = "420"
+  divs[2].style.opacity = 4.20;
+}
+//declare diffcolor() here
+function diffcolor(color, complement){
+  let color = "0000FF"
+  let complement = "FF8C00"
+  const doc = Document;
+  
+}
